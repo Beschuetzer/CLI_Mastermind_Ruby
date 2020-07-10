92ad947424b26dc07e178e2908db02fcf4452ab9
Adam Major <adam.j.major@gmail.com>
2020-07-09 21:19:00 -0700

    Update some formatting, add play_again, and refactor

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,6 @@
 require 'io/console'
-
+$max_pattern_length = 15
+$max_guesses = $max_pattern_length * 2
 
 class MastermindGame
   @@pattern_choosers = {
@@ -8,21 +9,14 @@ class MastermindGame
   }
   @@colors = %w(green white red brown magenta cyan)
 
-  def initialize(number_of_guesses, pattern_length)
-    raise "pattern_length must be 6 or lower" if pattern_length > 6
-    @number_of_guesses = number_of_guesses
-    @pattern_length = pattern_length  
-    @guesses = []
-    @feedbacks = []
-    @guesses_left = number_of_guesses
-    @is_a_winner = false
+  def initialize()
+    
   end
 
-  def play
-    @i = 0
-    display_instructions.include?('y') ? get_pattern(0) : get_pattern(1)
+  def play()
+    display_instructions_and_setup.include?('y') ? get_pattern(0) : get_pattern(1)    
     while true
-      puts "\nGUESSING PHASE:\n".send(@@colors[0]) if @i == 0
+      puts "\nGUESSING PHASE (Guesses: #{@guesses_left}):\n".send(@@colors[0]) if @i == 0
       get_guess
       if @guesses_left > 0 && @is_a_winner == false
         display_game
@@ -30,11 +24,24 @@ class MastermindGame
         break
       end
       @i += 1
-    end
-    puts "Codemakers prevail this time!\n"if @guesses_left <= 0
+    end    
+    display_game
+    print_results
+    get_play_again
   end
   
   private
+  def setup_variables(number_of_guesses, pattern_length)
+    raise "pattern_length must be #{$max_pattern_length} or lower" if pattern_length > $max_pattern_length
+    @number_of_guesses = number_of_guesses
+    @pattern_length = pattern_length  
+    @guesses_left = number_of_guesses
+    @guesses = []
+    @feedbacks = []
+    @is_a_winner = false
+    @i = 0
+  end
+
   def display_game
     puts "\nGuesses Remaining: #{@guesses_left}".send(@@colors[0])
     @guesses.each_index{|index1|
@@ -65,15 +72,21 @@ class MastermindGame
     print spacing_char
   end
 
-  def display_instructions
-    puts "\n\nMASTERMIND:\n".send(@@colors[0])
-    print "Would you like to play against the computer? "
-    ans = gets.chomp
-    while !ans.match(/^\s*[yYnN]([eE][sS]|[oO])*\s*$/) do                                  
-      print "Would you like to play against the computer?  Available options are 'y' and 'n': "
-      ans = gets.chomp.downcase
+   def print_pattern_in_color
+    @pattern.each{|color|
+        print_in_color(color, " ")
+    }
+    print "is the code.\n\n"
+  end
+
+  def print_results
+    puts "\n\n"
+    if @is_a_winner
+      puts "Codebreakers Win This Time!".red
+    elsif @guesses_left <= 0
+      puts "Codemakers Prevail This Time!".red
     end
-    ans
+    print_pattern_in_color
   end
 
   def get_guess
@@ -92,16 +105,7 @@ class MastermindGame
         break
       end
     }
-    if @is_a_winner
-      puts "Code breakers win this time!.  "
-      @pattern.each{|color|
-        print_in_color(color, " ")
-      }
-      print "is the code.\n\n"
-    else
-      puts "Not Quite. Keep on guessing."
-      get_guess_feedback
-    end
+    get_guess_feedback
   end
 
   def get_guess_feedback
@@ -113,13 +117,14 @@ class MastermindGame
         @correct_exactly_count += 1
       elsif @current_guess.any?{|item| item == @pattern[index]} 
         color_count = get_color_count_in_guess(@pattern[index])
+        puts "color: #{@pattern[index]} and color_count: #{color_count}"
         if (color_count >= colors_added_count[@pattern[index]] + 1)
           @correct_color_count += 1
           colors_added_count[@pattern[index]] += 1
         end
       end
     }
-    raise "@correct_color_count + @correct_exactly_count cannot be higher than 4.  Something went wrong" if @correct_color_count + @correct_exactly_count > 4
+    raise "@correct_color_count + @correct_exactly_count cannot be higher than #{@pattern_length}.  Something went wrong" if @correct_color_count + @correct_exactly_count > @pattern_length
     feedback = [@correct_exactly_count, @correct_color_count]
     @feedbacks.push(feedback)
   end
@@ -147,22 +152,20 @@ class MastermindGame
   end
 
   def get_human_responses(num_of_responses, msgPrefix, msgSuffix, hide)
-    # puts "\n"
     i = 1
     result = []
     valid_responses = []
     @@colors.each{|color| valid_responses += [color[0]]}
     num_of_responses.times{
-      # puts "i.to_s[i.to_s.length-1] #{i.to_s[i.to_s.length-1]}"
       case i.to_s[i.to_s.length-1]
-      when "1"
-        suffix = "st"
-      when "2"
-        suffix = "nd"
-      when "3"
-        suffix = "rd"
-      else        
-        suffix = "th"
+        when "1"
+          suffix = "st"
+        when "2"
+          suffix = "nd"
+        when "3"
+          suffix = "rd"
+        else        
+          suffix = "th"
       end
         print msgPrefix + "#{i}" + suffix + " color of the " + msgSuffix + "  Options are #{@@colors.to_sentence}: "
         ans = (hide == 1) ? STDIN.noecho(&:gets).chomp.strip : gets.chomp.strip
@@ -171,28 +174,65 @@ class MastermindGame
         print "That is not a valid option.  Options are #{@@colors.to_sentence}: "
         ans = (hide == 1) ? STDIN.noecho(&:gets).chomp.strip : gets.chomp.strip
       end
-
       case ans.downcase
-      when valid_responses[0]
-        ans = @@colors[0]
-      when valid_responses[1]
-        ans = @@colors[1]
-      when valid_responses[2]
-        ans = @@colors[2]
-      when valid_responses[3]
-        ans = @@colors[3]
-      when valid_responses[4]
-        ans = @@colors[4]
-      when valid_responses[5]
-        ans = @@colors[5]
+        when valid_responses[0]
+          ans = @@colors[0]
+        when valid_responses[1]
+          ans = @@colors[1]
+        when valid_responses[2]
+          ans = @@colors[2]
+        when valid_responses[3]
+          ans = @@colors[3]
+        when valid_responses[4]
+          ans = @@colors[4]
+        when valid_responses[5]
+          ans = @@colors[5]
       end
-
       result += [ans.downcase]
       i+=1
     }
     puts "\n"
     result
   end
+
+  def get_play_again
+    if yes_no_prompt("Play again? ").match('y')
+      play()
+    else
+      puts "\n"
+    end
+  end
+
+  def get_parameters
+    guesses = 0
+    while !guesses.to_i.between?(1,$max_guesses) do                                  
+      print "Maximum Number of Guesses? (1 - #{$max_guesses}): "
+      guesses = gets.chomp.downcase
+    end
+    pattern_length = 0
+    while !pattern_length.to_i.between?(1,$max_pattern_length) do                                  
+      print "Pattern Length (Number of Colors to guess)? (1 - #{$max_pattern_length}): "
+      pattern_length = gets.chomp.downcase
+    end
+    [guesses.to_i, pattern_length.to_i]
+  end
+
+  def yes_no_prompt(msg)
+    ans = ""
+    while !ans.match(/^\s*[yYnN]([eE][sS]|[oO])*\s*$/) do                                  
+      print msg + "  Available options are 'y' and 'n': "
+      ans = gets.chomp.downcase
+    end 
+    ans
+  end
+
+  def display_instructions_and_setup
+    puts "\n\nMASTERMIND:\n".send(@@colors[0])
+    parameters = get_parameters
+    setup_variables(parameters[0], parameters[1])
+    yes_no_prompt("Would you like to play against the computer?")
+  end
+
 end
 
 class String
@@ -241,5 +281,5 @@ class Array
   end
 end
 
-mastermind_game = MastermindGame.new(10, 4)
-mastermind_game.play
+mastermind_game = MastermindGame.new()
+mastermind_game.play()
